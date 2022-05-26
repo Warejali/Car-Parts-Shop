13cd82628eb82616d8f40a6218408a372650da41
Md Warej Ali <waraj.com@gmail.com>
2022-05-26 23:11:13 +0600

    few change

--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
+import logo from "../../assets/Logo.png"
 
 const Navbar = () => {
     const [user] = useAuthState(auth);
@@ -26,11 +27,12 @@ const Navbar = () => {
                     <label tabIndex="0" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
+                    <label for="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         {menuItems}
                     </ul>
                 </div>
-                <Link to="/" className="btn btn-ghost normal-case text-xl">MY-MOON AUTO</Link>
+                <Link to="/" className="btn btn-ghost normal-case text-xl"><img className='xs' src={logo} alt="" /></Link>
             </div>
             <div className="navbar-end hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
